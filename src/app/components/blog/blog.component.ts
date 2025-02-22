5e978353918ea5f23432ea9705ec544ea3e6e8cc
wikidev <santiago.rhp7@gmail.com>
2025-02-22 16:27:38 -0500

    create interface, create and initialization of array

--- src/app/components/blog/blog.component.ts
+++ src/app/components/blog/blog.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { INews } from '../../interfaces/inews.interface';
 
 @Component({
   selector: 'app-blog',
@@ -8,5 +9,8 @@ import { FormsModule } from '@angular/forms';
   styleUrl: './blog.component.css'
 })
 export class BlogComponent {
-
+  arrNews: INews[] = [
+    { title: 'The Substance Earns Five Academy Award Nominations', urlImg: 'https://npr.brightspotcdn.com/dims3/default/strip/false/crop/1100x619+0+0/resize/1200/quality/75/format/jpeg/?url=http%3A%2F%2Fnpr-brightspot.s3.amazonaws.com%2F6c%2F9a%2F8ead42f74b5fb070ad5b0b880afa%2Fimage-65.jpg', text: 'The movie, primarily relying on practical effects, centers on Elisabeth Sparkle (portrayed by Moore), a declining celebrity who takes an illicit drug that temporarily transforms her into a younger version of herself, Sue (played by Margaret Qualley), forcing her to deal with the consequences.', date: 'January 23, 2025' },  
+    { title: 'A Complete Unknown honors Bob Dylans life and musical journey', urlImg: 'https://lobbyobserver.org/wp-content/uploads/2025/01/A-Complete-Unknown-860x489-1.jpg', text: 'A Complete Unknown,” now nominated for eight Academy Awards, including Best Picture, explores the captivating yet imperfect journey of Dylans rise to fame. Timothée Chalamet takes on the role of Bob Dylan, showcasing both his acting and singing skills—a performance that earned him his second Oscar nomination.', date: 'February 20, 2025' },  
+    ]
 }
---
+++ src/app/interfaces/inews.interface.ts
@@ -0,0 +1,6 @@
+export interface INews {
+    title: string;
+    urlImg: string;
+    text: string;
+    date: string;
+}
