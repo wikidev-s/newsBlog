9bc66858857a4d4ea30e9c101c1ba5d1a5d3256a
wikidev <santiago.rhp7@gmail.com>
2025-02-22 15:03:17 -0500

    create blog component and add to app-root

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { BlogComponent } from "./components/blog/blog.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [BlogComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
